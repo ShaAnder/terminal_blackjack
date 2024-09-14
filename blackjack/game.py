ccde1ea588da2f89176f943660749646e3ec9324
Shaun Anderton <shaunanderton1@gmail.com>
2024-09-14 14:25:36 +0000

    added deck building function to game.py

--- blackjack/game.py
+++ blackjack/game.py
@@ -23,3 +23,13 @@ suit_values = {"Spades":"\u2664", "Hearts":"\u2661", "Clubs": "\u2667", "Diamond
 cards = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
 card_values = {"A": 11, "2":2, "3":3, "4":4, "5":5, "6":6, "7":7, "8":8, "9":9, "10":10, "J":10, "Q":10, "K":10}
 
+# now we want to create deck based on our suits values and cards
+def create_deck():
+  deck = []
+  for suit in suits:
+    for card in cards:
+      deck.append(Card(suit_values[suit], card, card_values[card]))
+  return deck
+
+deck = create_deck()
+print(deck)
