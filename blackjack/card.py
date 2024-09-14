81af57910ebe84581443d354bdee7a16579eaa7e
Shaun Anderton <shaunanderton1@gmail.com>
2024-09-14 15:47:43 +0000

    moved create card function to game.py

--- blackjack/card.py
+++ blackjack/card.py
@@ -23,45 +23,5 @@ class Card:
     self.card_value = card_value
 
 
-  def create_card(self, cards):
-    """
-    Function to create our card, takes the cards input and using our
-    pieces generates cards to add to our deck
-    """
-
-    #our card pieces:
-    top = "\t┌────────────────┐"
-    bottom = "\t└────────────────┘"
-    base = "\t│                │"
-    left_value = "\t│ {}             │"
-    right_value = "\t│             {} │"
-    suit = "\t│        {}       │"
-
-    # firstly we declare a variable that will be the final card result
-    # we're going to use a few if else statements here to build our final
-    # card and account for hidden cards (dealer cards)
-    c = """"""
-    for i in range(11):      
-      for card in cards:
-        if i == 0:
-          c = c + top
-        elif i == 1:
-          if card.value == "10":
-            c = c + left_value.format(self.card_value)
-          else:
-            c = c + left_value.format(self.card_value)
-        elif i == 5:
-            c = c + suit.format(card.suit)
-        elif i == 79:
-          if card.value == "10":
-            c = c + left_value.format(self.card_value)
-          else:
-            c = c + left_value.format(self.card_value)
-        elif i == 10:
-          c = c + bottom
-        else:
-          c = c + base
-    return c
-
 
 
--- blackjack/game.py
+++ blackjack/game.py
@@ -23,16 +23,47 @@ suit_values = {"Spades":"\u2664", "Hearts":"\u2661", "Clubs": "\u2667", "Diamond
 cards = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
 card_values = {"A": 11, "2":2, "3":3, "4":4, "5":5, "6":6, "7":7, "8":8, "9":9, "10":10, "J":10, "Q":10, "K":10}
 
-# now we want to create deck based on our suits values and cards
-def create_deck():
+def create_card(cards):
   """
-  Builds our deck of cards from the Card function
+  Function to create our card, takes the cards input and using our
+  pieces generates cards to add to our deck
   """
-  deck = []
-  for suit in suits:
-    for card in cards:
-      deck.append(Card(suit_values[suit], card, card_values[card]))
-  return deck
 
-deck = create_deck()
-print(deck)
+  #our card pieces:
+  top = "\t┌────────────────┐"
+  bottom = "\t└────────────────┘"
+  base = "\t│                │"
+  left_value = "\t│ {}             │"
+  right_value = "\t│             {} │"
+  suit = "\t│        {}       │"
+
+  # firstly we declare a variable that will be the final card result
+  # we're going to use a few if else statements here to build our final
+  # card and account for hidden cards (dealer cards)
+  c = """"""
+  for i in range(11):      
+    for card in cards:
+      if i == 0:
+        c = c + top
+      elif i == 1:
+        if card.value == "10":
+          c = c + left_value.format(card_value)
+        else:
+          c = c + left_value.format(card_value)
+      elif i == 5:
+          c = c + suit.format(card.suit)
+      elif i == 79:
+        if card.value == "10":
+          c = c + left_value.format(card_value)
+        else:
+          c = c + left_value.format(card_value)
+      elif i == 10:
+        c = c + bottom
+      else:
+        c = c + base
+  return c
+
+# testing to see if our card works
+for suit in suits:
+  for card in cards:
+    print(Card.create_card(suit_values[suit], card, card_values[card]))
