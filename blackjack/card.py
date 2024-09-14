92460e029334155a363ad92cd6c592014f03ccd3
Shaun Anderton <shaunanderton1@gmail.com>
2024-09-14 16:12:31 +0000

    added card / suit variables to run.py, added deck creation loop

--- blackjack/card.py
+++ blackjack/card.py
@@ -14,7 +14,7 @@ class Card:
   Class to create our card
   """
 
-  def __init__(self, suit,value,card_value):
+  def __init__(self, suit, value, card_value):
     #suit of the card we want to make
     self.suit = suit
     # the valeu of the card (1-10, A, K)
@@ -22,6 +22,52 @@ class Card:
     # score of the card 
     self.card_value = card_value
 
+  # added create card function to card class because it's logically to
+  # do with card creation
+  def create_card(self, cards, hidden):
+    """
+    Function to create our card, takes the cards input and using our
+    pieces generates cards to add to our deck
+    """
+
+    #our card pieces:
+    top = "\t┌────────────────┐"
+    bottom = "\t└────────────────┘"
+    base = "\t│                │"
+    left_value = "\t│ {}             │"
+    right_value = "\t│             {} │"
+    suit = "\t│        {}       │"
+
+
+    # this is our card building functionality, we want to 
+    # dynamically build a card if it's face up, UNLESS
+    # the card is hidden (dealer cards), if the card is hidden
+    # we instead return a blank card
+    c = """"""
+    for i in range(11):      
+      for card in cards:
+        if i == 0:
+          c = c + top
+        elif i == 1:
+          if card.value == "10":
+            c = c + left_value.format(card_value)
+          else:
+            c = c + left_value.format(card_value)
+        elif i == 5:
+            c = c + suit.format(card.suit)
+        elif i == 79:
+          if card.value == "10":
+            c = c + left_value.format(card_value)
+          else:
+            c = c + left_value.format(card_value)
+        elif i == 10:
+          c = c + bottom
+        else:
+          c = c + base
+    if hidden === True:
+      print(hidden_card)
+    else:
+      print(c)
 
 
 
--- blackjack/layout.py
+++ blackjack/layout.py
@@ -89,17 +89,17 @@ And remember the house always wins...
 
 # this is an example card, it will be dynamically created via a function and
 # inserted into a list for reading later
-card = """
+hidden_card = """
 ┌───────────────┐
+│ ?             │
 │               │
 │               │
 │               │
+│       ?       │
 │               │
 │               │
 │               │
-│               │
-│               │
-│               │
+│             ? │
 └───────────────┘                                                                   
 """
 
