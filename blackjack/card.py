f073513a5dc0e64f2ac49fd9877775c470f5f494
Shaun Anderton <shaunanderton1@gmail.com>
2024-09-17 11:08:58 +0000

    changed size of the cards

--- blackjack/card.py
+++ blackjack/card.py
@@ -32,20 +32,20 @@ class Card:
     """
     # Card pieces - using string formatting we align and format
     # each variable space to 2 spaces, to account for single digita
-    top = "┌───────────────┐"
-    bottom = "└───────────────┘"
-    base = "│               │"
-    left_value_format = "│ {:<2}            │"  # Left-align with width 2
-    right_value_format = "│            {:>2} │"  # Right-align with width 2
-    suit = "│       {}       │"
+    top = "┌─────────────┐"
+    bottom = "└─────────────┘"
+    base = "│             │"
+    left_value_format = "│ {:<2}          │"  # Left-align with width 2
+    right_value_format = "│          {:>2} │"  # Right-align with width 2
+    suit = "│      {}      │"
 
     # Create our card template of 11 entries
-    card_rows = [top, base, base, base, base, base, base, base, base, base, bottom]
+    card_rows = [top, base, base, base, base, base, base, base, bottom]
     # Apply string formatting to the row based on the card value
     card_rows[1] = left_value_format.format(card.value)
-    card_rows[9] = right_value_format.format(card.value) 
+    card_rows[7] = right_value_format.format(card.value) 
     # Format the suit
-    card_rows[5] = suit.format(card.suit)
+    card_rows[4] = suit.format(card.suit)
     # If hidden (dealer card) return the hidden card from layout
     if hidden:
         card_rows = hidden_card.split("\n")
