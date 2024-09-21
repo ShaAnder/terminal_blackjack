866d03b8770240d98524dd90d38beb47ee23a4ba
Shaun Anderton <shaunanderton1@gmail.com>
2024-09-21 10:49:16 +0000

    move loss / win user text down

--- blackjack/terminal_paint.py
+++ blackjack/terminal_paint.py
@@ -138,7 +138,7 @@ def win(condition_message):
   with term.location (0, 11):
     for line in layout.win_heading.split("\n"):
         print(term.yellow + term.center(line))
-  position(term.center(condition_message), 0, 15, term.black_on_yellow) 
+  position(term.center(condition_message), 0, 20, term.black_on_yellow) 
   # Ask player to play again
 
 
@@ -152,7 +152,7 @@ def loss(condition_message):
   with term.location (0, 11):
     for line in layout.loss_heading.split("\n"):
         print(term.yellow + term.center(line))
-  position(term.center(condition_message), 0, 15, term.black_on_yellow) 
+  position(term.center(condition_message), 0, 20, term.black_on_yellow) 
 
 def calculating(message):
   """
