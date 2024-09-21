47df3744182066c0aa324ad7e3c45c6d348f880d
Shaun Anderton <shaunanderton1@gmail.com>
2024-09-21 16:42:58 +0000

    fix: validated card.py

---
+++ TESTING.md
@@ -0,0 +1,37 @@
+# Testing
+
+Once the site was deployed there was extensive bug testing completed to ensure best functionality
+
+The deployed project live link is [Terminal Blackjack](https://terminal-blackjack-d1281da886b8.herokuapp.com)
+
+During the time testing I asked friends and family to test and thoroughly ensure the app worked. As it stands there are a few unsolved bugs
+theres are documented below
+
+The following tests were carried out to ensure that the game ran as expected
+
+| **Feature**              | **Action**                                                                | **Expected Result**                         | **Actual Result** |
+| ------------------------ | ------------------------------------------------------------------------- | ------------------------------------------- | ----------------- |
+| Game boot                | Game begins upon run start button                                         | Intro screens displayed as expected         | Works as expected |
+| User input functionality | Users are able to press any key to go through screens or control the game | User can play the game without issue        | Works as expected |
+| Validation               | User cannot enter anything except what's expected, doing so throws err    | Error message appears                       | Works as expected |
+| Gameplay                 | Gameplay loop works as intended                                           | Game play can be completed                  | Works as expected |
+| WIn / Loss conditions    | When game is played are correct screens displayed                         | Correct win / loss screen shows             | Works as expected |
+| Gameplay visuals         | Game displays cards and increments score                                  | Correct score incrementing and card visuals | Works as expected |
+
+## Testing Browsers
+
+The game was tested on a variety of browsers:
+
+- Chrome
+- Edge
+- Firefox
+- Oprea
+- Safari
+
+It worked without issues in the above browsers.
+
+## PEP8 Python Validation
+
+## Future Updates
+
+### [BACK TO README](https://github.com/shaAnder/terminal_blackjack/blob/main/README.md)
--- blackjack/card.py
+++ blackjack/card.py
@@ -4,49 +4,49 @@ card.py
 Main card class, this creates our blackjack card to append to the cards list
 """
 
-### --- IMPORTS --- ###
+# --- IMPORTS --- #
 
 # we want random to generate random cards ect
 import random
 
+
 class Card:
-  """
-  Class to create our card.
-  """
-
-  def __init__(self, suit, value, card_value):
-    #suit of the card we want to make
-    self.suit = suit
-    # the value of the card (1-10, A, K)
-    self.value = value
-    # score of the card 
-    self.card_value = card_value
-
-  # added create card function to card class because it's logically to
-  # do with card creation
-  def create_card(card):
     """
-    Function to create our card, takes the card's input and using our
-    pieces generates cards to add to our deck.
+    Class to create our card.
     """
-    # Card pieces - using string formatting we align and format
-    # each variable space to 2 spaces, to account for single digita
-    top = "┌───────────┐"
-    bottom = "└───────────┘"
-    base = "│           │"
-    left_value_format = "│ {:<2}        │"  # Left-align with width 2
-    right_value_format = "│        {:>2} │"  # Right-align with width 2
-    suit = "│     {}     │"
+    def __init__(self, suit, value, card_value):
+        # suit of the card we want to make
+        self.suit = suit
+        # the value of the card (1-10, A, K)
+        self.value = value
+        # score of the card
+        self.card_value = card_value
 
-    # Create our card template of 11 entries
-    card_rows = [top, base, base, base, base, base, base, base, bottom]
-    # Apply string formatting to the row based on the card value
-    card_rows[1] = left_value_format.format(card.value)
-    card_rows[7] = right_value_format.format(card.value) 
-    # Format the suit
-    card_rows[4] = suit.format(card.suit)
-    # If hidden (dealer card) return the hidden card from layout
-    return "\n".join(card_rows)
+    # added create card function to card class because it's logically to
+    # do with card creation
+    def create_card(card):
+        """
+        Function to create our card, takes the card's input and using our
+        pieces generates cards to add to our deck.
+        """
+        # Card pieces - using string formatting we align and format
+        # each variable space to 2 spaces, to account for single digita
+        top = "┌───────────┐"
+        bottom = "└───────────┘"
+        base = "│           │"
+        left_value_format = "│ {:<2}        │"  # Left-align with width 2
+        right_value_format = "│        {:>2} │"  # Right-align with width 2
+        suit = "│     {}     │"
+        # Create our card template of 11 entries
+        card_rows = [top, base, base, base, base, base, base, base, bottom]
+        # Apply string formatting to the row based on the card value
+        card_rows[1] = left_value_format.format(card.value)
+        card_rows[7] = right_value_format.format(card.value)
+        # Format the suit
+        card_rows[4] = suit.format(card.suit)
+        # If hidden (dealer card) return the hidden card from layout
+        return "\n".join(card_rows)
+
 
 
 
